c92f8a93bd704cb21febf313bfbe5d670cc94330
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2024-03-03 20:28:13 +0530

    get users
    get users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserForm from "./UserForm";
 import UserTable from "./UserTable";
-
-const users = [
-  { id: 1, name: "Sumedha" },
-  { id: 2, name: "Sankalpa" },
-];
+import  Axios  from "axios";
 
 const Users = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect (() => {
+    getUsers();
+  },[]);
+
+  const getUsers = () => {
+      Axios.get('http://localhost:3001/api/users')
+      .then(response => {
+        console.log(response.data.response)
+        setUsers(response.data.response)
+      })
+  }
+
   return (
     <React.Fragment>
       <UserForm />
