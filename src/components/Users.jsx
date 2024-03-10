8d7812c2785832b21043fefed69b155fab859f8e
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2024-03-10 11:49:17 +0530

    CRUD operations
    CRUD operations

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,28 +1,101 @@
 import React, { useEffect, useState } from "react";
 import UserForm from "./UserForm";
 import UserTable from "./UserTable";
-import  Axios  from "axios";
+import Axios from "axios";
 
 const Users = () => {
-
   const [users, setUsers] = useState([]);
+  const [submit, setSubmit] = useState([false]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [selectedUser, setSelectedUser] = useState({});
 
-  useEffect (() => {
+  useEffect(() => {
     getUsers();
-  },[]);
+  }, []);
 
+  //get users
   const getUsers = () => {
-      Axios.get('http://localhost:3001/api/users')
-      .then(response => {
-        console.log(response.data.response)
-        setUsers(response.data.response)
+    Axios.get("http://localhost:3001/api/users")
+      .then((response) => {
+        console.log(response.data.response);
+        setUsers(response.data?.response || []);
       })
+      .catch((error) => {
+        console.log("Axios Error :", error);
+      });
+  };
+
+  //add users
+  const addUser = (data) => {
+    setSubmit(true);
+
+    const payload = {
+      id: data.id,
+      name: data.name,
+    };
+
+    Axios.post("http://localhost:3001/api/createuser", payload)
+      .then(() => {
+        getUsers();
+        setSubmit(false);
+        isEdit(false);
+      })
+      .catch((error) => {
+        console.log("Axios Error :", error);
+      });
+  };
+
+  //update user
+  const updateUser = (data) => {
+    setSubmit(true);
+
+    const payload = {
+      id: data.id,
+      name: data.name,
+    };
+
+    Axios.put("http://localhost:3001/api/updateuser", payload)
+      .then(() => {
+        getUsers();
+        setSubmit(false);
+        isEdit(false);
+      })
+      .catch((error) => {
+        console.log("Axios Error :", Axios);
+      });
+  };
+
+  //Delete user
+  const deleteUser = (data) => {
+    Axios.delete("http://localhost:3001/api/deleteuser", data)
+      .then(() => {
+        getUsers();
+      })
+      .catch((error) => {
+        console.log("Axios Error :", Axios);
+      });
   }
 
   return (
     <React.Fragment>
-      <UserForm />
-      <UserTable rows={users}/>
+      <UserForm
+        addUser={addUser}
+        submit={submit}
+        updateUser={updateUser}
+        data={selectedUser}
+        isEdit={isEdit}
+      />
+      <UserTable
+        rows={users}
+
+        selectedUser={(data) => {
+          setSelectedUser(data);
+          setIsEdit(true);
+        }}
+
+        deleteUser={data => window.confirm('Are You Sure?') && deleteUser(data)}
+        
+      />
     </React.Fragment>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Users from "./components/Users.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />}/>
       <Route path='users' element={<Users />}/>
     </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+
 );
